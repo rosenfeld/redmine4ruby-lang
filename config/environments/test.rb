aa742993abef42342398b0ddd60ffe274abfe48a
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2008-08-11 18:58:13 +0900

    Email delivery configuration moved to an unversioned YAML file (config/email.yml, see the sample file) (#1412). Email delivery is disabled. It's automatically turned on when configuration is found.
    git-svn-id: svn://rubyforge.org/var/svn/redmine/trunk@1625 e93f8b46-1217-0410-a6f0-8f06a7374b81
    
    Conflicts:
    
    	config/environment.rb
    	lang/bg.yml
    	lang/cs.yml
    	lang/da.yml
    	lang/de.yml
    	lang/en.yml
    	lang/es.yml
    	lang/fi.yml
    	lang/fr.yml
    	lang/he.yml
    	lang/hu.yml
    	lang/it.yml
    	lang/ja.yml
    	lang/ko.yml
    	lang/lt.yml
    	lang/nl.yml
    	lang/no.yml
    	lang/pl.yml
    	lang/pt-br.yml
    	lang/pt.yml
    	lang/ro.yml
    	lang/ru.yml
    	lang/sr.yml
    	lang/sv.yml
    	lang/th.yml
    	lang/uk.yml
    	lang/zh.yml

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,7 @@ config.whiny_nils    = true
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
 
+config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :test
 
 require 'ruby-debug'
