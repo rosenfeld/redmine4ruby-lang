762252bb9f35bc82237678aa3c20d5a90bcad012
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2008-08-13 16:11:33 +0900

    reimplemented mail receiving daemon using plain TCPSocket.

--- lib/daemons/mail_receiver.rb
+++ lib/daemons/mail_receiver.rb
@@ -1,24 +1,29 @@
 #!/usr/bin/env ruby
 
-require 'drb/drb'
 require File.dirname(__FILE__) + "/../../config/environment"
 
 $running = true;
-Signal.trap("TERM") { $running = false }
+Signal.trap("TERM") { $running = false; throw :interrupted }
 logger = ActionMailer::Base.logger || ActiveRecord::Base.logger
 
 host = ARGV.shift
 port = ARGV.shift.to_i
 logger.info "daemon #{__FILE__} started at #{RAILS_ENV} environment"
-begin
-  DRb.start_service("druby://#{host}:#{port}", MailHandler)
-  logger.info "waiting at #{host}:#{port}"
-  while($running) do
-    logger.debug "daemon #{__FILE__} is still running at #{Time.now}."
-    sleep 10
-  end
-  logger.info "daemon #{__FILE__} stopped at #{RAILS_ENV} environment"
-ensure
-  bt = $!.backtrace.join("\n")
-  logger.error "#{$!.message}:#{bt}"
+server = TCPServer.new(host, port)
+while($running) do
+  catch (:interrupted) {
+    sock = server.accept
+    Thread.start do
+      begin
+        mail = sock.read
+        MailHandler.receive(mail) unless mail.empty?
+      rescue
+        bt = $!.backtrace.join("\n")
+        logger.error "#{$!.message}:#{bt}"
+      ensure
+        sock.close unless sock.closed?
+      end
+    end
+  }
 end
+logger.info "daemon #{__FILE__} stopped at #{RAILS_ENV} environment"
