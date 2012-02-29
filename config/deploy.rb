e3d932af5489f351c304888bd1932af8f51f0a78
NARUSE, Yui <naruse@airemix.jp>
2012-02-29 15:40:30 +0900

    Change repository.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'redmine'
 set :domain, 'fluorine.ruby-lang.org'
 set :deploy_to, '/var/lib/redmine'
-set :repository, 'git://github.com/yugui/redmine4ruby-lang.git'
+set :repository, 'git://github.com/asakusarb/redmine4ruby-lang.git'
 
 set :revision, 'origin/ruby-lang.org/1.1'
 
